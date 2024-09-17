4a8a9bf242fe787b6ba4ee91f53e49f745913eb7
Yanneck Dimitrov <yanneckdimitrov@gmail.com>
2024-09-17 13:19:57 +0200

    working hypergraph from file reader

--- main.cpp
+++ main.cpp
@@ -8,29 +8,40 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+
+NetworKit::Hypergraph readHypergraphFromFile(char* data) {
+    bool firstline = true;
+    NetworKit::Hypergraph hypergraph;
+    std::ifstream file;
+    file.open(data);
+    if(file.is_open()) {
+        std::string line;
+        while(std::getline(file, line)) {
+            if(firstline) {
+                hypergraph.addNodes(std::stoi(line));
+                firstline = false;
+            } else {
+                std::stringstream ss(line);
+                NetworKit::node node;
+                std::vector<NetworKit::node> nodes;
+                while (ss >> node) {
+                    nodes.push_back(node);
+                }
+                hypergraph.addEdge(nodes);
+            }
+        }
+    }
+    return hypergraph;
+}
 
 int main( int argc,      // Number of strings in array argv
           char *argv[],   // Array of command-line argument strings
           char *envp[] )  // Array of environment variable strings
 {
-    bool firstline = true;
-    NetworKit::Hypergraph hypergraph;
-    std::ifstream file;
-    file.open(argv[1]);
-    if(file.is_open()) {
-        std::string line;
-        while(file) {
-            std::getline(file, line);
-            std::cout << line << std::endl;
-            if(firstline) {
-                hypergraph.addNodes(line);
-            } else {
-                hypergraph.addEdge({line});
-            }
-        }
-    }
-
+    
+    NetworKit::Hypergraph hypergraph = readHypergraphFromFile(argv[1]);
     //print out hypergraph details
     std::cout << "Original Hypergraph:" << std::endl;
     std::cout << "Number of nodes: " << hypergraph.numberOfNodes() << std::endl;
